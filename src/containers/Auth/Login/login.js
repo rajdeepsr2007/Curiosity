86716fa736b0fff841b173ef3d750a2272aa434e
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-05-12 13:24:22 +0530

    Style Login

--- src/containers/Auth/Login/login.js
+++ src/containers/Auth/Login/login.js
@@ -1,8 +1,104 @@
 import React , { Component } from 'react';
+import {InputBase, TextField} from '@material-ui/core';
+import classes from './login.module.css';
+import {Button} from '@material-ui/core';
 
 class Login extends Component{
+
+    state={
+        controls:{
+            email : {
+                type : null,
+                label : 'Email or Username',
+                value : '',
+                isValid : false ,
+                touched : false,
+                error : null ,
+                validation : {
+                    rules : {
+                        isEmail : true ,
+                        isUnique : true
+                    }
+                }
+            },
+            password : {
+                type : 'password',
+                value : '',
+                label : 'Password',
+                isValid : false ,
+                touched : false,
+                error : null ,
+                validation : {
+                    rules : {
+                        isPassword : true
+                    }
+                }
+            }
+        },
+        isFormValid : false
+    }
+
+    inputChangeHandler = (event,inputKey) => {
+        this.setState( (prevState , props) => {
+            let updatedControls={};
+            for( const key in prevState.controls ){
+                updatedControls = {
+                    ...updatedControls,
+                    [key] : { ...prevState.controls[key] }
+                }
+            }
+            updatedControls[inputKey].value = event.target.value;
+            updatedControls[inputKey].touched = true;
+            updatedControls[inputKey].valid = updatedControls[inputKey].value!=='';
+
+            let isFormValid = true;
+            for( const key in this.state.controls ){
+                if( !updatedControls[key].valid ){
+                    isFormValid = false;
+                }
+            }
+            return {
+                ...prevState ,
+                controls : updatedControls,
+                isFormValid
+            }
+        } )
+    }
+
     render(){
-        return <h1></h1>
+
+        const formInputs = [];
+        for( let key in this.state.controls ){
+            const input = this.state.controls[key];
+            formInputs.push(
+                <div className={classes.input} key={key} >
+                    <TextField
+                    type={input.type}
+                    value={input.value}
+                    error={input.error}
+                    label={input.label}
+                    onChange={ (event) => this.inputChangeHandler(event,key) }
+                    />
+                </div>
+            )
+        }
+
+        const submitButton = <div className={classes.button} >
+                <Button 
+                variant="contained" 
+                color="primary"
+                disabled={!this.state.isFormValid}>Login</Button>
+            </div>
+
+        return(
+            <div className={classes.form} >
+                <form onSubmit={this.submitFormHandler} >
+                    {formInputs}
+                    {submitButton}
+                </form>  
+            </div>
+            
+        )
     }
 }
 
--- src/containers/Auth/auth.js
+++ src/containers/Auth/auth.js
@@ -3,12 +3,23 @@ import Login from './Login/login';
 import Signup from './Signup/signup';
 import classes from './auth.module.css';
 import Logo from '../../components/UI/Logo/logo';
+import {Button} from '@material-ui/core';
+import EmailIcon from '@material-ui/icons/MailOutlineRounded'
 
 class Auth extends Component{
 
     state={
         //Tells the mode of Auth page whether to login or signup
         isLogin : true
+    }
+
+    switchAuthModeHandler = () => {
+        this.setState( ( prevState , props ) => {
+            return {
+                prevState ,
+                isLogin : !prevState.isLogin
+            }
+        } )
     }
 
     render(){
@@ -24,6 +35,12 @@ class Auth extends Component{
                         <div className={classes.inputs} >
                             <Logo />
                             {content}
+                            <div className={classes.button} >
+                                <Button variant="outlined" onClick={this.switchAuthModeHandler} >
+                                    {this.state.isLogin ? <EmailIcon /> : null}
+                                    {this.state.isLogin ? <span style={{ marginLeft : '5px' }} > Signup With Email</span> : 'Switch To Login'}
+                                </Button>
+                            </div>
                         </div>
                     </div>
                </div>
