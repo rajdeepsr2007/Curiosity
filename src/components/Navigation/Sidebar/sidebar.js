7d70f171d8da417cc9d75df03a4420cb321d70ed
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-05-18 22:50:20 +0530

    Add links to responsive sidebar

--- src/components/Navigation/Sidebar/sidebar.js
+++ src/components/Navigation/Sidebar/sidebar.js
@@ -1,6 +1,9 @@
 import React , { Fragment } from 'react';
 import Backdrop from '../../UI/Modal/Backdrop/backdrop';
-import classes from './sidebar.module.css'
+import {NavLink} from 'react-router-dom';
+import {HomeOutlined,QuestionAnswerOutlined,CreateOutlined,Add,ExploreOutlined,List} from '@material-ui/icons';
+import classes from './sidebar.module.css';
+import Logo from '../../UI/Logo/logo';
 
 const Sidebar = (props) => {
 
@@ -8,7 +11,55 @@ const Sidebar = (props) => {
         <Fragment>
             <Backdrop show={true} onClick={props.onClick} />
             <div className={classes.sidebar} onClick={props.onClick}  >
-                
+            <div className={classes.logo} >
+                <Logo />
+            </div>
+            <div className={classes.nav} >
+                    <ul>
+                        <li>
+                            <NavLink to="/user/edit-topics" exact activeClassName={classes.active} >
+                                <div className={classes.link} >
+                                    <HomeOutlined /> Home
+                                </div>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/home" exact>
+                                <div className={classes.link} >
+                                    <QuestionAnswerOutlined /> Add Question
+                                </div>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/home" exact>
+                                <div className={classes.link} >
+                                    <CreateOutlined /> Answer
+                                </div>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/home" exact>
+                                <div className={classes.link} >
+                                    <List /> Following
+                                </div>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/home" exact>
+                                <div className={classes.link} >
+                                    <Add /> Add Space
+                                </div>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/home" exact>
+                                <div className={classes.link} >
+                                    <ExploreOutlined /> Discover Spaces
+                                </div>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </Fragment>
         
